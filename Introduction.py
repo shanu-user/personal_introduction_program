7b27095a63aea5f93b9c1117b32ac0134aa5b378
shanu-user <shanunandanwar12@gmail.com>
2026-03-12 22:05:56 +0530

    Posted every file

---
+++ Introduction.py
@@ -0,0 +1,27 @@
+# Day-1: Write Hello World
+
+print("Hello World")
+
+# Day-2: Add User Input:
+
+print("What is your name?")
+name = input()
+print("How old are you?")
+age = int(input())
+print("What is your favorite hobby?")
+hobby = input()
+
+# Day-3 Welcome Message
+
+print(f"🎉 Welcome {name}! 🎉")
+print(f"You are {age} years old and love {hobby}.")
+
+# Day-4 Additional Questions
+
+print("What's your favorite programming language?")
+prog_lang = input()
+print("Do you prefer working in the morning or at night?")
+shift = input()
+
+print(f"Programming Language: {prog_lang}")
+print(f'Shift: {shift}')
